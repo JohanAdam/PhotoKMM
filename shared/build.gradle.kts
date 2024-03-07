a52e34514c6db0f5e15bd131408318dee6a667e7
Johan <johanadam95@gmail.com>
2024-03-07 12:28:50 +0800

    - Add shared dependencies.

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -1,6 +1,9 @@
 plugins {
     alias(libs.plugins.kotlinMultiplatform)
     alias(libs.plugins.androidLibrary)
+
+    //Kotlinx Serialization.
+    kotlin("plugin.serialization") version "1.8.0"
 }
 
 kotlin {
@@ -26,9 +29,24 @@ kotlin {
     sourceSets {
         commonMain.dependencies {
             //put your multiplatform dependencies here
+            implementation(libs.kotlinx.coroutines.core)
+            implementation(libs.ktor.client.core)
+            implementation(libs.ktor.client.content.negotiation)
+            implementation(libs.ktor.serialization.kotlinx.json)
+
+            //Use api so that the android app can use it as well
+            api(libs.koin.core)
         }
         commonTest.dependencies {
             implementation(libs.kotlin.test)
+        }
+        androidMain.dependencies {
+            implementation(libs.ktor.client.android)
+
+            api(libs.koin.android)
+        }
+        iosMain.dependencies {
+            implementation(libs.ktor.client.darwin)
         }
     }
 }
