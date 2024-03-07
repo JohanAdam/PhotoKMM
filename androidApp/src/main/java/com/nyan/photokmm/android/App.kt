9d549965192ed50428b8bd1958ab034055443daf
Johan <johanadam95@gmail.com>
2024-03-07 21:57:40 +0800

    - Create Application class. - Add INTERNET permission. - Add compose app. - Connect ComposeApp to Activity. - Create AppModule.

--- androidApp/src/main/java/com/nyan/photokmm/android/App.kt
+++ androidApp/src/main/java/com/nyan/photokmm/android/App.kt
@@ -1,72 +1,17 @@
 package com.nyan.photokmm.android
 
-import androidx.compose.foundation.isSystemInDarkTheme
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.SideEffect
-import androidx.compose.runtime.getValue
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
-import androidx.navigation.compose.currentBackStackEntryAsState
-import androidx.navigation.compose.rememberNavController
-import com.google.accompanist.systemuicontroller.rememberSystemUiController
-import com.nyan.photokmm.android.common.AppBar
-import com.nyan.photokmm.android.common.Dashboard
-import com.nyan.photokmm.android.common.photoDestinations
-import com.nyan.photokmm.android.dashboard.DashboardScreen
-import com.nyan.photokmm.android.dashboard.DashboardViewModel
-import org.koin.androidx.compose.koinViewModel
+import android.app.Application
+import com.nyan.photokmm.android.di.appModule
+import com.nyan.photokmm.di.getSharedModules
+import org.koin.core.context.startKoin
 
-@Composable
-fun App() {
-    val navController = rememberNavController()
-    val systemUiNavController = rememberSystemUiController()
+class App: Application() {
 
-    val isSystemDark = isSystemInDarkTheme()
-    val statusBarColor = if (isSystemDark) {
-        MaterialTheme.colorScheme.tertiary
-    } else {
-        Color.Transparent
-    }
-    SideEffect {
-        systemUiNavController.setStatusBarColor(statusBarColor, darkIcons = !isSystemDark)
-    }
-
-    val backStackEntry by navController.currentBackStackEntryAsState()
-    val currentScreen = photoDestinations.find {
-        backStackEntry?.destination?.route == it.route || backStackEntry?.destination?.route == it.routeWithArgs
-    } ?: Dashboard
-
-    Scaffold(
-        topBar = {
-            AppBar(
-                canNavigateBack = navController.previousBackStackEntry != null,
-                currentScreen = currentScreen
-            ) {
-                navController.navigateUp()
-            }
-        }
-    ) { innerPaddings ->
-        NavHost(
-            navController = navController,
-            modifier = Modifier.padding(innerPaddings),
-            startDestination = Dashboard.routeWithArgs
-        ) {
-            composable(Dashboard.routeWithArgs) {
-                val dashboardViewModel: DashboardViewModel = koinViewModel()
-                DashboardScreen(
-                    uiState = dashboardViewModel.uiState,
-                    loadNextPhotos = {
-                        dashboardViewModel.loadPhotos(forceReload = it)
-                    },
-                    navigateToDetail = {
-
-                    })
-            }
+    override fun onCreate() {
+        super.onCreate()
+        startKoin {
+            modules(appModule + getSharedModules())
         }
     }
+
 }
---
+++ androidApp/src/main/java/com/nyan/photokmm/android/di/AppModule.kt
@@ -0,0 +1,9 @@
+package com.nyan.photokmm.android.di
+
+import com.nyan.photokmm.android.dashboard.DashboardViewModel
+import org.koin.androidx.viewmodel.dsl.viewModel
+import org.koin.dsl.module
+
+val appModule = module {
+    viewModel { DashboardViewModel(get()) }
+}
