c9d872084e3405a4a8f7506af12dd3ff35eb0238
Johan <johanadam95@gmail.com>
2024-03-08 19:28:55 +0800

    - Code cleanup list item compose. - Add image loading indicator.

--- androidApp/src/main/java/com/nyan/photokmm/android/dashboard/PhotoListItem.kt
+++ androidApp/src/main/java/com/nyan/photokmm/android/dashboard/PhotoListItem.kt
@@ -12,15 +12,14 @@ import androidx.compose.material3.Card
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
-import coil.compose.AsyncImage
 import com.nyan.photokmm.android.Purple
+import com.nyan.photokmm.android.common.image.AsyncImage
 import com.nyan.photokmm.domain.model.Photo
 
 @Composable
@@ -30,61 +29,47 @@ fun PhotoListItem(
     isSelected: Boolean,
     onPhotoClick: (Photo) -> Unit
 ) {
-    Box(
-        modifier = modifier.height(260.dp)
-    ) {
+    val parentItemRadius = 18.dp
+
+    Box(modifier = modifier.height(260.dp)) {
         Card(modifier = modifier
+            .fillMaxSize()
+            .clip(shape = RoundedCornerShape(parentItemRadius))
             .clickable { onPhotoClick.invoke(photo) }) {
 
             Column {
                 // ==========================
                 // Top Image Section.
                 // ==========================
-                Box(
-                    modifier = modifier.weight(1f),
-                    contentAlignment = Alignment.Center
-                ) {
-                    AsyncImage(
-                        model = photo.url,
-                        contentDescription = photo.title,
-                        contentScale = ContentScale.Crop,
-                        modifier = modifier
-                            .fillMaxSize()
-                            .clip(
-                                RoundedCornerShape(
-                                    bottomStart = 2.dp,
-                                    bottomEnd = 2.dp
-                                )
-                            )
-                    )
-                }
+                AsyncImage(
+                    url = photo.url,
+                    contentDescription = photo.title,
+                    contentScale = ContentScale.Crop,
+                    modifier = modifier
+                        .weight(1f)
+                )
 
                 // ==========================
                 // Bottom Title Section.
                 // ==========================
-                Column(
-                    modifier = modifier.padding(10.dp)
-                ) {
-                    Text(
-                        text = photo.title,
-                        style = MaterialTheme.typography.titleMedium,
-                        fontWeight = FontWeight.Bold,
-                        maxLines = 2,
-                        minLines = 2,
-                        overflow = TextOverflow.Ellipsis
-                    )
-                }
+                Text(
+                    modifier = modifier.padding(10.dp),
+                    text = photo.title,
+                    style = MaterialTheme.typography.titleMedium,
+                    fontWeight = FontWeight.Bold,
+                    maxLines = 2,
+                    minLines = 2,
+                    overflow = TextOverflow.Ellipsis
+                )
             }
         }
 
         // Overlay highlight with the same shape as the card
         if (isSelected) {
-            Box(
-                modifier = Modifier
-                    .fillMaxSize()
-                    .background(Purple.copy(alpha = 0.3f))
-                    .clip(shape = RoundedCornerShape(8.dp)) // Clip the overlay highlight
-            )
+            Box(modifier = Modifier
+                .fillMaxSize()
+                .clip(shape = RoundedCornerShape(parentItemRadius)) // Clip the overlay highlight
+                .background(Purple.copy(alpha = 0.4f)))
         }
     }
 }
